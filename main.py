32a1603eedbb26a7127f622844ca73efe1bb247a
minupogle-arch <minupogle@gmail.com>
2025-11-05 09:27:06 +0900

    Fix string concatenation in warning message

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ a=st.text_input('이름을 입력해주세요')
 b=st.selectbox('좋아하는 음식을 선택해세요!',['한식','중식','일식','양식','디저트'])
 if st.button('인사말 생성'):
   st.info(a+'님, 안녕하세요! 반갑습니다!')
-  st.warning(b='을 좋아하시는군요! 저도 좋아해요!')
+  st.warning(b+'을 좋아하시는군요! 저도 좋아해요!')
   st.error('반가워요!!')
   st.balloons()
