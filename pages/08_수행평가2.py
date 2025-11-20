32947a6e0cb5b713c02d49b95d7234e0bcd6c2d3
minupogle-arch <minupogle@gmail.com>
2025-11-20 09:21:46 +0900

    Update 08_수행평가2.py

--- pages/08_수행평가2.py
+++ pages/08_수행평가2.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+import matplotlib
 
 # -------------------------------
 # Streamlit 페이지 설정
@@ -36,24 +37,32 @@ else:
     top10 = df.sort_values(by=target_col, ascending=False).head(10)
 
     # -------------------------------
+    # matplotlib 폰트 설정 (영어 글씨 깨짐 방지)
+    # -------------------------------
+    matplotlib.rcParams['font.family'] = 'DejaVu Sans'  # 영어 기본 글꼴 명시
+    matplotlib.rcParams['axes.labelsize'] = 12
+    matplotlib.rcParams['xtick.labelsize'] = 10
+    matplotlib.rcParams['ytick.labelsize'] = 10
+
+    # -------------------------------
     # 막대그래프 그리기
     # -------------------------------
     fig, ax = plt.subplots(figsize=(10,6))
     
     # 파란색 그라데이션
-    colors = cm.Blues_r([i/10 for i in range(10)])  # 0~1 비율
+    colors = cm.Blues_r([i/10 for i in range(10)])
     ax.bar(top10.index.astype(str), top10[target_col], color=colors)
     
     # x축, y축 라벨
-    ax.set_xlabel("Index", fontsize=12)
-    ax.set_ylabel(target_col, fontsize=12)
+    ax.set_xlabel("Index")
+    ax.set_ylabel(target_col)
     
-    # y축 글씨 깨짐 최소화
-    ax.tick_params(axis='y', labelsize=10)
-    ax.tick_params(axis='x', rotation=45, labelsize=10)
+    # x축 글씨 회전
+    ax.tick_params(axis='x', rotation=45)
 
-    # 그래프 제목 제거
+    # 제목 제거 (깨짐 방지)
     # ax.set_title(...) 삭제
 
     plt.tight_layout()
     st.pyplot(fig)
+
